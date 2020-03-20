300cd99efe5b6f93364c6de9708314c296be2cad
David Speck <speckd@informatik.uni-freiburg.de>
2020-03-20 14:35:44 +0100

    exp fine tune

--- experiments/start_all.sh
+++ experiments/start_all.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 python train.py barman/ barman 3000
+sleep 5
 python train.py blocksworld/ blocksworld 3100
+sleep 5
 python train.py childsnack/ childsnack 3200
-python train.py gripper/ gripper 3300
-python train.py logistics/ logistics 3400
-python train.py rovers/ rovers 3500
-python train.py satellite/ satellite 3600
-python train.py sokoban/ sokoban 3700
-python train.py transport/ transport 3800
-python train.py visitall/ visitall 3900
+sleep 5
+python train.py logistics/ logistics 3300
+sleep 5
+python train.py rovers/ rovers 3400
+sleep 5
+python train.py satellite/ satellite 3500
+sleep 5
+python train.py sokoban/ sokoban 3600
+sleep 5
+python train.py transport/ transport 3700
+sleep 5
+python train.py visitall/ visitall 3800
 
